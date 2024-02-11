00119bd648dce0ac919fb253af45a4009eb65bd9
ValentinAmazing <podrezva@bk.ru>
2024-02-11 10:51:59 +0700

    Initial Commit

---
+++ Lesson 2.06 HW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 2.06 HW
+//
+//  Created by Valentin on 11.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
